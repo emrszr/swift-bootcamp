f2ac1e73afca990593ee908baa90863b752134cf
emrszr <emerszr@gmail.com>
2024-11-15 23:58:33 +0300

    Initial Commit

---
+++ Bootcamp/BootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  BootcampApp.swift
+//  Bootcamp
+//
+//  Created by Sezer Emer on 15.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct BootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
